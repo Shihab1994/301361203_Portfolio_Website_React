dbc275d0f9802b00877f084b5afb9fd3c1f08831
APU_Mahfuzul <mahfuzul@eatlbd.com>
2024-01-28 14:09:36 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./index.css";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Welcome to My React App</h1>
+      <p>This is a static paragraph in my web page.</p>
+    </div>
+  );
+};
+
+export default App;
