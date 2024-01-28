715946c621c4c508ebdfc9286d899e4fa11a0a23
APU_Mahfuzul <mahfuzul@eatlbd.com>
2024-01-28 14:53:17 -0500

    folder structure updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import "./index.css";
+import {Header} from './coponents/header/Header'
 
 const App = () => {
   return (
-    <div>
-      <h1>Welcome to My React App</h1>
-      <p>This is a static paragraph in my web page.</p>
-    </div>
+    <>
+    <Header/>
+    </>
+    // <div>
+    //   <h1>Welcome to My React App</h1>
+    //   <p>This is a static paragraph in my web page.</p>
+    // </div>
   );
 };
 
---
+++ src/coponents/testimonials/Testimonial.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './experience.css'
+
+export const testimonial = () => {
+  return (
+    <div>testimonial</div>
+  )
+}
