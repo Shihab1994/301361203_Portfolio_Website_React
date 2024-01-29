bc275f5bd1e668b770b78f7f32adaed767c6b828
APU_Mahfuzul <mahfuzul@eatlbd.com>
2024-01-29 02:06:14 -0500

    fixing issues

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,3 +1,24 @@
+// module.exports = {
+//   root: true,
+//   env: { browser: true, es2020: true },
+//   extends: [
+//     'eslint:recommended',
+//     'plugin:react/recommended',
+//     'plugin:react/jsx-runtime',
+//     'plugin:react-hooks/recommended',
+//   ],
+//   ignorePatterns: ['dist', '.eslintrc.cjs'],
+//   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+//   settings: { react: { version: '18.2' } },
+//   plugins: ['react-refresh'],
+//   rules: {
+//     'react-refresh/only-export-coponents': [
+//       'warn',
+//       { allowConstantExport: true },
+//     ],
+//   },
+// }
+
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
@@ -12,9 +33,9 @@ module.exports = {
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-coponents': [
+    'react-refresh/only-require-for-constants': [
       'warn',
       { allowConstantExport: true },
     ],
   },
-}
+};
