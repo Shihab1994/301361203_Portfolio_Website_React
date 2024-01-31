e2971680963d1cac3e0bd3c0c279d4338cfe5e4e
APU_Mahfuzul <mahfuzul@eatlbd.com>
2024-01-31 12:31:20 -0500

    make the whole box clickable in contact

--- src/coponents/contact/contact.jsx
+++ src/coponents/contact/contact.jsx
@@ -14,28 +14,25 @@ import { FaWhatsapp } from "react-icons/fa";
 import { useRef } from "react";
 import emailjs from "emailjs-com";
 
-
 export const Contact = () => {
   const form = useRef();
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs
-      .sendForm(
-        "service_mrdblar",
-        "template_xxfnqrh",
-        form.current,
-        "l12GZPijdMofD6o-a"
-      )
-      e.target.reset()
-      .then(
-        (result) => {
-          console.log(result.text);
-        },
-        (error) => {
-          console.log(error.text);
-        }
-      );
+    emailjs.sendForm(
+      "service_mrdblar",
+      "template_xxfnqrh",
+      form.current,
+      "l12GZPijdMofD6o-a"
+    );
+    e.target.reset().then(
+      (result) => {
+        console.log(result.text);
+      },
+      (error) => {
+        console.log(error.text);
+      }
+    );
   };
   return (
     <section id="contact">
@@ -45,29 +42,29 @@ export const Contact = () => {
       <div className="container contact__container">
         <div className="contact__options">
           <article className="contact__option">
-            <MdOutlineEmail className="contact__option-icon" />
-            <h4>Email</h4>
-            <h5>mahfuzulislam15@gmail.com</h5>
             <a href="mailto:mahfuzulislam15@gmail.com" target="_blank">
+              <MdOutlineEmail className="contact__option-icon" />
+              <h4>Email</h4>
+              <h5>mahfuzulislam15@gmail.com</h5>
               Send a message
             </a>
           </article>
           <article className="contact__option">
-            <RiMessengerLine className="contact__option-icon" />
-            <h4>Messenger</h4>
-            <h5>Facebook</h5>
             <a href="https://m.me/Shihab96/" target="_blank">
+              <RiMessengerLine className="contact__option-icon" />
+              <h4>Messenger</h4>
+              <h5>Facebook</h5>
               Send a message
             </a>
           </article>
           <article className="contact__option">
-            <FaWhatsapp className="contact__option-icon" />
-            <h4>WhatsApp</h4>
-            <h5>+14164588366</h5>
             <a
               href="https://api.whatsapp.com/send?phone=14164588366"
               target="_blank"
             >
+              <FaWhatsapp className="contact__option-icon" />
+              <h4>WhatsApp</h4>
+              <h5>+14164588366</h5>
               Send a message
             </a>
           </article>
